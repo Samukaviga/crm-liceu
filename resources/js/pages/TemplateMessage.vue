dedda13650a69d04660244914961954f7b021d91
samuel <samuelgomes101112@gmail.com>
2026-01-14 09:25:31 -0300

    adding alerts components

--- resources/js/pages/TemplateMessage.vue
+++ resources/js/pages/TemplateMessage.vue
@@ -2,59 +2,9 @@
 
     <AppLayout>
 
-         <div v-if="showSuccess" id="alert-1"
-            class="flex items-center p-4 mx-5 mt-5 mb-2 text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-blue-400"
-            role="alert">
-            <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 20">
-                <path
-                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-            </svg>
-            <span class="sr-only">Info</span>
-            <div class="ms-3 text-sm font-medium">
-                {{ success }}
-            </div>
+        <SuccessMessage v-if="showSuccess && success" :message="success" @close="closeAlert" />
 
-            <button @click="closeAlert" type="button"
-                class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700"
-                data-dismiss-target="#alert-1" aria-label="Close">
-                <span class="sr-only">Close</span>
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-            </button>
-        </div>
-
-
-
-
-        <div v-if="statusError" id="alert-1"
-            class="flex items-center p-4 mx-5 mt-5 mb-2 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-blue-400"
-            role="alert">
-            <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 20">
-                <path
-                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-            </svg>
-            <span class="sr-only">Info</span>
-            <div class="ms-3 flex flex-col gap-2 text-sm font-medium">
-                <p v-for="msg in messageError" :key="msg">{{ msg }}</p>
-            </div>
-
-            <button @click="closeAlert" type="button"
-                class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700"
-                data-dismiss-target="#alert-1" aria-label="Close">
-                <span class="sr-only">Close</span>
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-            </button>
-        </div>
-
+        <ErrorMessage v-if="statusError" :message="messageError" @close="closeAlert" />
 
         <section class="bg-white py-8 antialiased dark:bg-black md:py-16">
             <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
@@ -98,7 +48,6 @@
                                 <Input type="text" v-model="registerForm.number" />
                             </div>
 
-
                             <button type="submit" :disabled="loading"
                                 class="flex w-full items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-900 dark:hover:bg-blue-800 dark:focus:ring-blue-800">
                                 {{ loading ? 'Enviando...' : 'Importar' }}
@@ -129,26 +78,26 @@
 
 import { reactive, ref, computed } from 'vue'
 import AppLayout from '@/layouts/AppLayout.vue'
-//import axios from 'axios'
-
 import { Input } from '@/components/ui/input'
 import { usePage, router } from '@inertiajs/vue3'
+import SuccessMessage from '@/components/ui/alert/SuccessMessage.vue'
+import ErrorMessage from '@/components/ui/alert/ErrorMessage.vue'
+import Button from '@/components/ui/button/Button.vue'
+
+
 const { props } = usePage()
 const page = usePage()
-
-const showSuccess = ref(false)
+const schools = props.schools;
+const showSuccess = ref(true)
 
 const success = computed(() =>
     showSuccess.value ? page.props.flash?.success : null
 )
 
-
 let loading = ref(false);
 let messageError = ref('');
 let statusError = ref(false);
 
-
-const schools = props.schools;
 
 type Template = {
     school_id: number | null;
