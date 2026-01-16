b380ece6b95236f684783634c956319a76885e76
samuel <samuelgomes101112@gmail.com>
2026-01-16 16:44:57 -0300

    adding table ui to template

--- resources/js/pages/TemplateList.vue
+++ resources/js/pages/TemplateList.vue
@@ -10,6 +10,51 @@
                         Lista de Templates
                     </h2>
 
+
+                    <div class="flex w-full flex-col gap-6">
+
+                        <Table>
+                            <TableCaption>A list of your recent invoices.</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead class="w-[100px]">
+                                        Invoice
+                                    </TableHead>
+                                    <TableHead>Status</TableHead>
+                                    <TableHead>Method</TableHead>
+                                    <TableHead class="text-right">
+                                        Amount
+                                    </TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <TableRow v-for="invoice in invoices" :key="invoice.invoice">
+                                    <TableCell class="font-medium">
+                                        {{ invoice.invoice }}
+                                    </TableCell>
+                                    <TableCell>{{ invoice.paymentStatus }}</TableCell>
+                                    <TableCell>{{ invoice.paymentMethod }}</TableCell>
+                                    <TableCell class="text-right">
+                                        {{ invoice.totalAmount }}
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    <TableCell colspan="3">
+                                        Total
+                                    </TableCell>
+                                    <TableCell class="text-right">
+                                        $2,500.00
+                                    </TableCell>
+                                </TableRow>
+                            </TableFooter>
+                        </Table>
+
+                    </div>
+
+
+
                     <div class="mt-6 sm:mt-8 lg:flex lg:items-center lg:gap-12">
 
 
@@ -28,7 +73,62 @@
 
 <script setup lang="ts">
 
-    import AppLayout from '@/layouts/AppLayout.vue'
+import AppLayout from '@/layouts/AppLayout.vue'
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table'
+const invoices = [
+    {
+        invoice: 'INV001',
+        paymentStatus: 'Paid',
+        totalAmount: '$250.00',
+        paymentMethod: 'Credit Card',
+    },
+    {
+        invoice: 'INV002',
+        paymentStatus: 'Pending',
+        totalAmount: '$150.00',
+        paymentMethod: 'PayPal',
+    },
+    {
+        invoice: 'INV003',
+        paymentStatus: 'Unpaid',
+        totalAmount: '$350.00',
+        paymentMethod: 'Bank Transfer',
+    },
+    {
+        invoice: 'INV004',
+        paymentStatus: 'Paid',
+        totalAmount: '$450.00',
+        paymentMethod: 'Credit Card',
+    },
+    {
+        invoice: 'INV005',
+        paymentStatus: 'Paid',
+        totalAmount: '$550.00',
+        paymentMethod: 'PayPal',
+    },
+    {
+        invoice: 'INV006',
+        paymentStatus: 'Pending',
+        totalAmount: '$200.00',
+        paymentMethod: 'Bank Transfer',
+    },
+    {
+        invoice: 'INV007',
+        paymentStatus: 'Unpaid',
+        totalAmount: '$300.00',
+        paymentMethod: 'Credit Card',
+    },
+]
 
 
 </script>
